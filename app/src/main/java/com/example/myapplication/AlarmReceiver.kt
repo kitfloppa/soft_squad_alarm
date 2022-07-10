68b386480a25540465c12692547212e8a893e6a6
kitfloppa <timofeev.anik@students.dvfu.ru>
2022-07-11 00:35:56 +1000

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/AlarmReceiver.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.net.Uri
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        val i = Intent(context, DestinationActivity::class.java)
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(context, 0, i, 0)
+
+        var alarmUri: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        }
+        val ringtone = RingtoneManager.getRingtone(context, alarmUri)
+
+        var builder = NotificationCompat.Builder(context!!, "kitfloppa")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Kitfloppa's Alarm Manager")
+            .setContentText("Vezdecode THE BEST")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(123, builder.build())
+        ringtone.play()
+    }
+}
